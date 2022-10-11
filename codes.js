44281cd94c41ced077bdbbd2e24a288391ce5fea
Ashutosh Tiwari <ashutoshtiwari22111998@gmail.com>
2022-10-11 20:47:50 +0530

    addSpiner35

--- codes.js
+++ codes.js
@@ -1740,8 +1740,8 @@ const codes = [
         `,
   },
   {
-        id: 29,
-        code: `
+    id: 29,
+    code: `
         ##HTML
         <div class="loader-29">
                     <div class="wave"></div>
@@ -1813,10 +1813,10 @@ const codes = [
         } 
             
             `,
-      },
-      {
-        id: 30,
-        code: `
+  },
+  {
+    id: 30,
+    code: `
        ##HTML
        <div class="loader-box">
         <div class="loader-30">
@@ -1864,10 +1864,10 @@ const codes = [
     };
     
             `,
-      },
-      {
-        id: 31,
-        code: `
+  },
+  {
+    id: 31,
+    code: `
        ##HTML
        <div class="loader-box">
        <div class="loader-31">
@@ -2155,9 +2155,9 @@ const codes = [
    
 
         `,
-},
-{
-  
+  },
+  {
+
     id: 33,
     code: `
    ##HTML
@@ -2224,5 +2224,57 @@ const codes = [
 
         `,
 
+  },
+
+  {
+
+    id: 35,
+    code: `
+ ##HTML
+ 
+ <div class="loader-35">
+        <div class="signal">
+          <div></div>
+        </div>
+      </div>
+  
+##CSS
+.signal {
+  width: 40px;
+  height: 40px;
+  margin: 100px auto;
+  background-color: #333;
+
+  border-radius: 100%;
+  -webkit-animation: sk-scaleout 1.0s infinite ease-in-out;
+  animation: sk-scaleout 1.0s infinite ease-in-out;
 }
+
+@-webkit-keyframes sk-scaleout {
+  0% {
+    -webkit-transform: scale(0)
+  }
+
+  100% {
+    -webkit-transform: scale(1.0);
+    opacity: 0;
+  }
+}
+
+@keyframes sk-scaleout {
+  0% {
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+
+  100% {
+    -webkit-transform: scale(1.0);
+    transform: scale(1.0);
+    opacity: 0;
+  }
+}
+
+      `,
+
+  }
 ];
