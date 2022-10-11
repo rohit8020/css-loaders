cb32f34d83fe9e0f28801db68388bdf9709c913a
Ashutosh Tiwari <ashutoshtiwari22111998@gmail.com>
2022-10-11 21:20:59 +0530

    Adding Spinner 36

--- codes.js
+++ codes.js
@@ -2336,5 +2336,58 @@ const codes = [
       }
     }
     `
+  },
+
+  {
+    id: 36,
+    code:
+      `
+    #Loader-36
+  
+    ##HTML
+  
+    <div class="loader-36">
+        <div class="signal">
+          <div></div>
+        </div>
+      </div>
+    
+  
+    ##CSS
+    
+    .signal {
+      width: 40px;
+      height: 40px;
+      background-color: #333;
+    
+      border-radius: 100%;
+      -webkit-animation: sk-scaleout 1.0s infinite ease-in-out;
+      animation: sk-scaleout 1.0s infinite ease-in-out;
+    }
+    
+    @-webkit-keyframes sk-scaleout {
+      0% {
+        -webkit-transform: scale(0)
+      }
+    
+      100% {
+        -webkit-transform: scale(1.0);
+        opacity: 0;
+      }
+    }
+    
+    @keyframes sk-scaleout {
+      0% {
+        -webkit-transform: scale(0);
+        transform: scale(0);
+      }
+    
+      100% {
+        -webkit-transform: scale(1.0);
+        transform: scale(1.0);
+        opacity: 0;
+      }
+    }
+    `
   }
 ];
