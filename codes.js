1b040e9de90ba824a53939b38b980a23125da42c
Gopalakrishnan <krishnan.gopal34@gmail.com>
2022-10-15 02:25:00 +0530

    style: :rocket: Added a simple loader with text

--- codes.js
+++ codes.js
@@ -2735,4 +2735,62 @@ const codes = [
 }
     `,
   },
+  {
+    id: 41,
+    code: `
+      ##HTML
+      <div class="loader41"></div>
+      
+      ##CSS
+      .loader41{
+        position: relative;
+        width: 150px;
+        height: 20px;
+        background-color: rgba(255,255,255,0.2);
+      }
+      
+      .loader41:before{
+        content: "";
+        position: absolute;
+        background-color: #fff;
+        top: 0px;
+        left: 0px;
+        height: 20px;
+        width: 0px;
+        z-index: 0;
+        opacity: 1;
+        -webkit-transform-origin:  100% 0%;
+            transform-origin:  100% 0% ;
+        -webkit-animation: loader41 10s ease-in-out infinite;
+            animation: loader41 10s ease-in-out infinite;
+      }
+      
+      .loader41:after{
+        content: "LOADING ...";
+        color: #fff;
+        font-weight: 200;
+        font-size: 16px;
+        position: absolute;
+        width: 100%;
+        height: 20px;
+        line-height: 20px;
+        left: 0;
+        top: 0;
+      }
+      
+      @-webkit-keyframes loader41{
+          0%{width: 0px;}
+          70%{width: 100%; opacity: 1;}
+          90%{opacity: 0; width: 100%;}
+          100%{opacity: 0;width: 0px;}
+      }
+      
+      @keyframes loader41{
+          0%{width: 0px;}
+          70%{width: 100%; opacity: 1;}
+          90%{opacity: 0; width: 100%;}
+          100%{opacity: 0;width: 0px;}
+      }
+    `
+  }
 ];
